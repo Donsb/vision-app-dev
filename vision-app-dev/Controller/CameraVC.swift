1893141a24d39a5ec38c5cea8777211366679b83
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-22 14:00:19 -0800

    Created new Group Folders: Controller, View and Resources.  Renamed VC to CameraVC.  Moved CameraVC to Controller folder.  General Code cleanup.

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CameraVC.swift
 //  vision-app-dev
 //
 //  Created by Donald Belliveau on 2017-12-22.
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CameraVC: UIViewController {
+    
+    // Functions
+    
+    // View Did Load.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    } // END View Did Load.
+    
+    
+    // Did Receive Memory Warning.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
-}
+    } // END Did Receive Memory Warning.
+    
+    
+} // END Class.
 
