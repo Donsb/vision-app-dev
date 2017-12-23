3073e19283dd341e166f5ef0dda7f433d29cdf5b
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-22 16:41:32 -0800

    Added Function Did Tap Camera View.  Added extension Protocol AVCapturePhotoCaptureDelegate with it's method: Photo Output.  Added UITapGestureRecognizer.

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -16,6 +16,7 @@ class CameraVC: UIViewController {
     var captureSession: AVCaptureSession!
     var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var photoData: Data?
     
     // IBOutlets
     
@@ -45,6 +46,10 @@ class CameraVC: UIViewController {
     // View Will Appear.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
+        
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
         
@@ -66,6 +71,7 @@ class CameraVC: UIViewController {
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 
                 cameraView.layer.addSublayer(previewLayer!)
+                cameraView.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
         } catch {
@@ -74,9 +80,37 @@ class CameraVC: UIViewController {
     } // END View Will Appear.
     
     
+    // Did Tap Camera View.
+    func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first! // first is basic iOS photo
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
+        
+        settings.previewPhotoFormat = previewFormat
+        
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    } // END Did Tap Camera View.
+    
+    
 } // END Class.
 
-
+//
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    
+    // Photo Output.
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        } else {
+            photoData = photo.fileDataRepresentation()
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+        
+    } // END Photo Output.
+    
+}
 
 
 
